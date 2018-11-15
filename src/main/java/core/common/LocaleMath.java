e2a4d7e70f3877350aabf00cf739fcd64e9cde90
Arthur <apploidyakutsk@gmail.com>
2018-11-15 14:10:30 +0900

    initial commit

---
+++ src/main/java/core/common/LocaleMath.java
@@ -0,0 +1,19 @@
+package core.common;
+
+/**
+ * Local Math
+ * <br>
+ * Подстроенные под специфические нужды класс, для работы с методами {@link java.lang.Math}
+ */
+public class LocaleMath {
+
+    /**
+     * Возвращает случайное число в диапозоне от <code>minValue</code> до <code>maxValue</code>
+     * @param minValue нижняя граница (включительно)
+     * @param maxValue верхняя граница (включительно)
+     * @return рандомное число в указанном диапозоне
+     */
+    public static int randInt(int minValue, int maxValue){
+        return (int)(java.lang.Math.random()*maxValue) + minValue;
+    }
+}
