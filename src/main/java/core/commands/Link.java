7c18ee81fdda64c1d9966a54986289fff422ba13
Arthur <apploidyakutsk@gmail.com>
2018-12-12 21:37:28 +0300

    changed returns queue (sorted)

--- src/main/java/core/commands/Link.java
+++ src/main/java/core/commands/Link.java
@@ -8,12 +8,17 @@ import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.TreeMap;
 
+/**
+ * @author Артур Куприянов
+ * @version 1.2
+ */
 public class Link extends Command {
     private JSONObject jsonObjectData;
 
     @Override
-    void setName() {
+    protected void setName() {
         name = "link";
     }
 
@@ -25,26 +30,34 @@ public class Link extends Command {
         }
     }
 
+    /**
+     * Ключи: <br>
+     *  -o | --opd - журнал по ОПД <br>
+     *  -p | --programming - журнал по программирвоанию <br>
+     *  -i | --informatics - журнал по информатике <br>
+     */
     @Override
     public String init(String... args) {
 
-        Map<String, String> keysMap = KeysReader.readKeys(args);
         StringBuilder response = new StringBuilder();
-
-        if(keysMap.containsKey("--opd") || keysMap.containsKey("-o")){
-            response.append("ОПД : ");
-            response.append(jsonObjectData.get("opd").toString());
-            response.append("\n");
-        }
-        if(keysMap.containsKey("--informatics") || keysMap.containsKey("-i")){
-            response.append("Информатика : ");
-            response.append(jsonObjectData.get("informatics").toString());
-            response.append("\n");
-        }
-        if(keysMap.containsKey("--programming") || keysMap.containsKey("-p")){
-            response.append("Программирование : ");
-            response.append(jsonObjectData.get("programming").toString());
-            response.append("\n");
+        TreeMap<Integer, Map<String, String>> keysMap = KeysReader.readOrderedKeys(args);
+        for (int key: keysMap.keySet()
+             ) {
+            if (keysMap.get(key).containsKey("--opd") || keysMap.get(key).containsKey("-o")) {
+                response.append("ОПД : ");
+                response.append(jsonObjectData.get("opd").toString());
+                response.append("\n");
+            }
+            if (keysMap.get(key).containsKey("--informatics") || keysMap.get(key).containsKey("-i")) {
+                response.append("Информатика : ");
+                response.append(jsonObjectData.get("informatics").toString());
+                response.append("\n");
+            }
+            if (keysMap.get(key).containsKey("--programming") || keysMap.get(key).containsKey("-p")) {
+                response.append("Программирование : ");
+                response.append(jsonObjectData.get("programming").toString());
+                response.append("\n");
+            }
         }
 
         if (response.length() == 0){
