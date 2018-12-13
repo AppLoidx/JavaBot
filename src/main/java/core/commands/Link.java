f1ac650b76bc0be40ab75ca26b61959e210eb9c3
Arthur <apploidyakutsk@gmail.com>
2018-12-13 15:56:44 +0300

    Integrated work with Database (SQLite)

--- src/main/java/core/commands/Link.java
+++ src/main/java/core/commands/Link.java
@@ -3,19 +3,22 @@ package core.commands;
 
 import core.common.JSONReader;
 import core.common.KeysReader;
+import core.modules.LinksDB.LinksDB;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Map;
 import java.util.TreeMap;
 
 /**
  * @author Артур Куприянов
- * @version 1.2
+ * @version 2.0
  */
 public class Link extends Command {
-    private JSONObject jsonObjectData;
+
+    private LinksDB linksDB;
 
     @Override
     protected void setName() {
@@ -23,18 +26,11 @@ public class Link extends Command {
     }
 
     Link(){
-        try {
-            jsonObjectData = JSONReader.getJSONObject("src/main/botResources/links.json");
-        } catch (ParseException | IOException e) {
-            e.printStackTrace();
-        }
+             linksDB = new LinksDB();
     }
 
     /**
-     * Ключи: <br>
-     *  -o | --opd - журнал по ОПД <br>
-     *  -p | --programming - журнал по программирвоанию <br>
-     *  -i | --informatics - журнал по информатике <br>
+     * Возвращает ссылки по ключу
      */
     @Override
     public String init(String... args) {
@@ -43,20 +39,17 @@ public class Link extends Command {
         TreeMap<Integer, Map<String, String>> keysMap = KeysReader.readOrderedKeys(args);
         for (int key: keysMap.keySet()
              ) {
-            if (keysMap.get(key).containsKey("--opd") || keysMap.get(key).containsKey("-o")) {
-                response.append("ОПД : ");
-                response.append(jsonObjectData.get("opd").toString());
-                response.append("\n");
-            }
-            if (keysMap.get(key).containsKey("--informatics") || keysMap.get(key).containsKey("-i")) {
-                response.append("Информатика : ");
-                response.append(jsonObjectData.get("informatics").toString());
-                response.append("\n");
-            }
-            if (keysMap.get(key).containsKey("--programming") || keysMap.get(key).containsKey("-p")) {
-                response.append("Программирование : ");
-                response.append(jsonObjectData.get("programming").toString());
-                response.append("\n");
+            try {
+                String DBKey = String.valueOf(keysMap.get(key).keySet().toArray()[0]);
+                String link = linksDB.getLinkByKey(DBKey);
+                if (link!=null){
+                    response.append(linksDB.getNameByKey(DBKey));
+                    response.append("\n");
+                    response.append(link);
+                    response.append("\n");
+                }
+            } catch (SQLException | ClassNotFoundException e) {
+                e.printStackTrace();
             }
         }
 
