7d1b3dc1ffae84370652c61202ada19e56c3235b
Arthur Desk <apploidyakutsk@gmail.com>
2019-02-22 22:13:57 +0300

    added network connection exception catch and deleted useless imports

--- src/main/java/vk/VKServer.java
+++ src/main/java/vk/VKServer.java
@@ -1,25 +1,17 @@
 package vk;
 
-import com.vk.api.sdk.client.TransportClient;
-import com.vk.api.sdk.client.VkApiClient;
-import com.vk.api.sdk.client.actors.GroupActor;
+
 import com.vk.api.sdk.exceptions.ApiException;
 import com.vk.api.sdk.exceptions.ClientException;
-import com.vk.api.sdk.httpclient.HttpTransportClient;
-import com.vk.api.sdk.objects.messages.Message;
-import com.vk.api.sdk.objects.users.UserCounters;
 import com.vk.api.sdk.objects.users.UserXtrCounters;
-import com.vk.api.sdk.queries.messages.MessagesGetLongPollHistoryQuery;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.List;
-import java.util.Properties;
+import java.io.*;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class VKServer {
-    public static VKCore  vkCore;
+    public static VKCore vkCore;
 
     static {
         try {
@@ -29,18 +21,24 @@ public class VKServer {
         }
     }
 
-    public static void main(String[] args) throws NullPointerException, ClientException, ApiException, InterruptedException, IOException {
+    public static void main(String[] args) throws NullPointerException, ApiException, InterruptedException, IOException {
 
         while (true) {
             Thread.sleep(300);
-            String[] message = vkCore.getMessage();
-            if (!message[0].equals("Error")) {
-                UserXtrCounters userInfo = vkCore.getUserInfo(message[1]);
-                ExecutorService exec = Executors.newCachedThreadPool();
-                exec.execute(new Messenger(vkCore.getActor(), vkCore.getVk(), message[0], Integer.valueOf(message[1]), userInfo));
+            try {
+                String[] message = vkCore.getMessage();
+                if (!message[0].equals("Error")) {
+                    UserXtrCounters userInfo = vkCore.getUserInfo(message[1]);
+                    ExecutorService exec = Executors.newCachedThreadPool();
+                    exec.execute(new Messenger(vkCore.getActor(), vkCore.getVk(), message[0], Integer.valueOf(message[1]), userInfo));
+                }
+            } catch (ClientException e) {
+                System.out.println("Нет интернет соединения");
+                final int RECONNECT_TIME = 10000;
+                System.out.println("Повторное соединение через " + RECONNECT_TIME / 1000 + " секунд");
+                Thread.sleep(RECONNECT_TIME);
+
             }
-
         }
-
     }
 }
