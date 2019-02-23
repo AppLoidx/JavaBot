98443ea574870cf2f8d956bae0c26fa2beb8443e
Arthur Desk <apploidyakutsk@gmail.com>
2019-02-23 15:14:45 +0300

    added new meta data: program

--- src/main/java/core/common/UserInfoReader.java
+++ src/main/java/core/common/UserInfoReader.java
@@ -3,15 +3,19 @@ package core.common;
 import java.util.Map;
 
 /**
- * @version 1.0
+ * @version 1.1
  * @author Arthur Kupriyanov
  */
 public class UserInfoReader {
+    private final static String META_DATA_IDENTIFICATION = "--#";
+
     public static String readUserID(String ... args){
         Map<String, String> keysMap = KeysReader.readKeys(args);
 
-        if (keysMap.containsKey("--#user_id")){
-            return keysMap.get("--#user_id");
+        String metaDataKey = META_DATA_IDENTIFICATION + "user_id";
+
+        if (keysMap.containsKey(metaDataKey)){
+            return keysMap.get(metaDataKey);
         }
 
         return null;
@@ -19,8 +23,10 @@ public class UserInfoReader {
     public static String readUserFirstName(String ... args){
         Map<String, String> keysMap = KeysReader.readKeys(args);
 
-        if (keysMap.containsKey("--#first_name")){
-            return keysMap.get("--#first_name");
+        String metaDataKey = META_DATA_IDENTIFICATION + "first_name";
+
+        if (keysMap.containsKey(metaDataKey)){
+            return keysMap.get(metaDataKey);
         }
 
         return null;
@@ -28,11 +34,22 @@ public class UserInfoReader {
     public static String readUserLastName(String ... args){
         Map<String, String> keysMap = KeysReader.readKeys(args);
 
-        if (keysMap.containsKey("--#last_name")){
-            return keysMap.get("--#last_name");
+        String metaDataKey = META_DATA_IDENTIFICATION + "last_name";
+
+        if (keysMap.containsKey(metaDataKey)){
+            return keysMap.get(metaDataKey);
         }
 
         return null;
+    }
+
+    public static boolean checkIsProgramm(String ... args){
+        Map<String, String> keysMap = KeysReader.readKeys(args);
+
+        String metaDataKey = META_DATA_IDENTIFICATION + "program";
+
+        return keysMap.containsKey(metaDataKey);
+
     }
 
 
