806c1091dbaa298349f34c7b8adce36a61a13623
Arthur Desk <apploidyakutsk@gmail.com>
2019-02-24 21:32:16 +0300

    added auth methods

--- src/main/java/core/modules/UsersDB.java
+++ src/main/java/core/modules/UsersDB.java
@@ -1,17 +1,22 @@
 package core.modules;
 
-import org.apache.logging.log4j.core.appender.db.jdbc.ConnectionSource;
-
 import java.sql.*;
 
 /**
  * @author Arthur Kupriyanov
  */
+@SuppressWarnings("Duplicates")
 public class UsersDB {
-    final String FIRST_NAME = "first_name";
-    final String LAST_NAME = "last_name";
-    final String VKID = "vkid";
-    final String GROUP_NAME = "group_name";
+
+    //ИМЕНА КОЛОНОК
+
+    private final String FIRST_NAME = "first_name";
+    private final String LAST_NAME = "last_name";
+    private final String VKID = "vkid";
+    private final String GROUP_NAME = "group_name";
+    private final String LOGIN = "login";
+    private final String PASSWORD = "password";
+
     private Connection connection;
 
     public UsersDB() throws SQLException {
@@ -21,6 +26,13 @@ public class UsersDB {
         this.connection = DriverManager.getConnection(dbUrl + log + additionalConfig);
     }
 
+    /**
+     * Добавление пользователя в базу данных
+     * @param firstName Имя
+     * @param lastname Фамилия
+     * @param vkid номер от VK ID
+     * @param groupName номер группы (необязательный параметр)
+     */
     public void addUser(String firstName, String lastname, int vkid, String groupName) throws SQLException {
         Statement statement;
         statement = connection.createStatement();
@@ -30,6 +42,11 @@ public class UsersDB {
         statement.executeUpdate(sqlStatement);
     }
 
+    /**
+     * Проверяет существование пользователя через номер от VK ID
+     * @param vkid номер VK ID
+     * @return <code>true</code> если есть, иначе <code>false</code>
+     */
     public boolean checkUserExsist(int vkid) throws SQLException {
         Statement statement;
         statement = connection.createStatement();
@@ -44,6 +61,12 @@ public class UsersDB {
         }
         return false;
     }
+
+    /**
+     * Получает полное имя, используя номер ID от VK
+     * @param vkid номер ID от VK
+     * @return номер группы
+     */
     public String getFullNameByVKID(int vkid) throws SQLException {
 
         Statement statement;
@@ -62,6 +85,12 @@ public class UsersDB {
 
         return null;
     }
+
+    /**
+     * Получает номер группы, используя номер ID от VK
+     * @param vkid номер ID от VK
+     * @return номер группы
+     */
     public String getGroupByVKID(int vkid) throws SQLException {
         Statement statement;
         statement = connection.createStatement();
@@ -82,5 +111,103 @@ public class UsersDB {
 
     public void closeConnection() throws SQLException {
         connection.close();
+    }
+
+    /**
+     * Обновление\Добавление Логина и Пароля у пользователя
+     * @param login логин
+     * @param password пароль
+     * @param VKID номер ID от VK
+     */
+    public void updateUserLoginPassword(String login, String password, int VKID) throws SQLException {
+        Statement statement = connection.createStatement();
+        String sql = "UPDATE users SET login='"+login+"', password='"+password+"' WHERE vkid="+VKID;
+        statement.execute(sql);
+    }
+
+    /**
+     * Обновление колонки группа в базе данных
+     * @param group номер группы
+     * @param vkid номер ID от VK
+     */
+    public void updateGroup(String group, int vkid) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement("UPDATE users SET group_name = ? WHERE vkid = ?");
+        statement.setString(1,group);
+        statement.setInt(2,vkid);
+        statement.execute();
+    }
+    public boolean isAuthorized(int vkid) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT login, password, vkid FROM users");
+        while(resultSet.next()){
+            int dbVKID = resultSet.getInt(VKID);
+            if (dbVKID == vkid) {
+                try {
+                    String loginDB = resultSet.getString(LOGIN).replace(" ", "");
+                    String passwordDB = resultSet.getString(PASSWORD).replace(" ", "");
+                } catch (NullPointerException e) {
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * -2 - пользователь не авторизован
+     * -1 - пользователь не найден
+     * @param login логин
+     * @param password пароль
+     * @return код ошибки или ID пользователя
+     */
+    public int auth(String login, String password) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT login, password, vkid FROM users");
+        while(resultSet.next()){
+            int dbVKID = resultSet.getInt(VKID);
+            if (!isAuthorized(dbVKID)){
+                continue;
+            }
+            String loginDB = resultSet.getString(LOGIN).replace(" ", "");
+            String passwordDB = resultSet.getString(PASSWORD).replace(" ", "");
+
+            System.out.println(loginDB + " " + passwordDB);
+            if (loginDB.equals(login) && passwordDB.equals(password)){
+                return dbVKID;
+            }
+        }
+        return -1;
+    }
+
+    public boolean auth(String login, String password, int vkid) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT login, password, vkid FROM users");
+        if (!isAuthorized(vkid)){
+            return false;
+        }
+        while(resultSet.next()){
+            int dbVKID = resultSet.getInt(VKID);
+            if (vkid == dbVKID) {
+                String loginDB = resultSet.getString(LOGIN).replace(" ", "");
+                String passwordDB = resultSet.getString(PASSWORD).replace(" ", "");
+
+                if (loginDB.equals(login) && passwordDB.equals(password) && dbVKID == vkid) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) throws SQLException {
+        UsersDB usersDB = new UsersDB();
+        //usersDB.addUser("Arthur","Kupriyanov",1200, "P3112");
+        //usersDB.addUser("Daniel","Marshall",1222, "P3113");
+        System.out.println(usersDB.isAuthorized(1200));
+        System.out.println(usersDB.auth("apploid","12345",1200));
+        usersDB.closeConnection();
+
     }
 }
