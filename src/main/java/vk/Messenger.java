d1c7426527d5a7847f445c404ebf92efd172e95d
Arthur Desk <apploidyakutsk@gmail.com>
2019-03-02 16:32:10 +0300

    added javadoc and new method getUserInfo

--- src/main/java/vk/Messenger.java
+++ src/main/java/vk/Messenger.java
@@ -21,6 +21,21 @@ public class Messenger implements Runnable{
         this.peerId = message.getUserId();
     }
 
+    /**
+     * Обращается к классу {@link Commander} сначала с аргументом типа {@link Message},
+     * если такой команды не найдется (выполнится команда {@link core.commands.Unknown},
+     * которая возвращает null - выполнится обычная команда с методом init(String) c
+     * с дополнинением к сигнатуре метаданных
+     *
+     * @return В случае успешного выполнения - строку, в случае, если команда не
+     * нуждается в автоматической отправке ответа ( либо он осуществляется внутри
+     * самой программы)  - <code>null</code>. Исключительным случаем является возвращение
+     * ответа от команды {@link core.commands.Unknown}, которая возвращает строковый ответ о том,
+     * что команда не найдена.
+     * @see Commander
+     * @see core.CommandDeterminant
+     * @see core.commands.Unknown
+     */
     private String getResponse(){
 
         String vkResponse =  Commander.getResponse(message);
@@ -48,8 +63,34 @@ public class Messenger implements Runnable{
         }
     }
 
+    /**
+     * Получаем ответ методом {@link #getResponse()} и в случае, если он
+     * не <code>null</code> - отправляем как простое сообщение.
+     */
     @Override
     public void run() {
-        sendMessage(getResponse());
+        String response = getResponse();
+        if (response!=null) {
+            sendMessage(response);
+        }
+    }
+
+    /**
+     * Обращается к VK API и получает объект, описывающий пользователя.
+     * @param id идентификатор пользователя в VK
+     * @return {@link UserXtrCounters} информацию о пользователе
+     * @see UserXtrCounters
+     */
+    private UserXtrCounters getUserInfo(int id){
+        try {
+            return vk.users()
+                    .get(actor)
+                    .userIds(String.valueOf(id))
+                    .execute()
+                    .get(0);
+        } catch (ApiException | ClientException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
