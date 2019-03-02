02a600d668254906588a34267ad5b77398557694
Arthur Desk <apploidyakutsk@gmail.com>
2019-03-02 16:26:09 +0300

    new method getResponse with Message type for VKCommand specified commands

--- src/main/java/core/Commander.java
+++ src/main/java/core/Commander.java
@@ -1,12 +1,18 @@
 package core;
 
-import core.commands.CommandDeterminant;
-import core.commands.CommandManager;
+import com.vk.api.sdk.objects.messages.Message;
 
 public class Commander {
 
     public static String getResponse(String userInput){
         return CommandDeterminant.getCommand(CommandManager.getCommands(), userInput).init(userInput);
+    }
+
+    public static String getResponse(Message message){
+        return CommandDeterminant.getVKCommand(CommandManager.getCommands(), message).exec(message);
+    }
+    public static void main(String[] args) {
+        System.out.println(Commander.getResponse("user -l marshall -p myLongPass"));
     }
 
 }
