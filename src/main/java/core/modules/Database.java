7bf26db6b8ce7601e4a1ffda078828399393b5c7
Arthur Desk <apploidyakutsk@gmail.com>
2019-03-03 19:19:19 +0300

    changed from full static class to non-static class

---
+++ src/main/java/core/modules/Database.java
@@ -0,0 +1,40 @@
+package core.modules;
+
+import com.sun.javaws.exceptions.ExitException;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * @author Arthur Kupriyanov
+ */
+public class Database {
+    private Connection connection;
+    {
+        Properties dbConfig = new Properties();
+        try {
+            dbConfig.load(new FileReader(new File("src/main/java/core/modules/herokuDatabaseConfig.properties")));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Файл с конфигурацией базы данных не найден!");
+            System.exit(ExitException.LAUNCH_ABORT_SILENT);
+        }
+        String dbUrl = dbConfig.getProperty("dbURL");
+        String log = dbConfig.getProperty("log");
+        String additionalConfig = dbConfig.getProperty("config");
+        try {
+            connection = DriverManager.getConnection(dbUrl + log + additionalConfig);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+}
