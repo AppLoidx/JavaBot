a8dde74219f8fc98acfb7a5125f05428f5b3b9bc
Arthur Desk <apploidyakutsk@gmail.com>
2019-03-05 02:44:50 +0300

    new data structure class for images

---
+++ src/main/java/core/modules/res/MenheraSprite.java
@@ -0,0 +1,18 @@
+package core.modules.res;
+
+/**
+ * @author Arthur Kupriyanov
+ */
+public class MenheraSprite {
+    public static final String HI_SPRITE = "photo-172998024_456239022";
+    public static final String GOOD_MORNING_SPRITE = "photo-172998024_456239023";
+    public static final String LOVELY_SPRITE = "photo-172998024_456239024";
+    public static final String HEHE_SPRITE = "photo-172998024_456239025";
+    public static final String THINKING_SPRITE = "photo-172998024_456239026";
+    public static final String HIDDEN_SPRITE = "photo-172998024_456239027";
+    public static final String READY_SPRITE = "photo-172998024_456239028";
+    public static final String WHAT_SPRITE = "photo-172998024_456239029";
+    public static final String MM_SPRITE = "photo-172998024_456239030";
+    public static final String ABORING_SPRITE = "photo-172998024_456239031";
+
+}
