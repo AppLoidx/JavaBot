ad0a11671ea7d4b54494a7740003d507a5949e10
Arthur Desk <apploidyakutsk@gmail.com>
2019-03-04 22:02:12 +0300

    added virtual env config import

--- src/main/java/core/modules/Database.java
+++ src/main/java/core/modules/Database.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -16,16 +17,25 @@ public class Database {
     private Connection connection;
     {
         Properties dbConfig = new Properties();
+        String dbUrl;
+        String log;
+        String additionalConfig;
         try {
             dbConfig.load(new FileReader(new File("src/main/java/core/modules/herokuDatabaseConfig.properties")));
+            dbUrl = dbConfig.getProperty("dbURL");
+            log = dbConfig.getProperty("log");
+            additionalConfig = dbConfig.getProperty("config");
         } catch (IOException e) {
+            Map<String, String> env = System.getenv();
+            dbUrl = env.get("CUSTOM_DATABASE_URL");
+            log = env.get("CUSTOM_DATABASE_LOG");
+            additionalConfig = env.get("CUSTOM_DATABASE_CONFIG");
+            System.out.println(env);
             e.printStackTrace();
             System.err.println("Файл с конфигурацией базы данных не найден!");
             System.exit(4);
         }
-        String dbUrl = dbConfig.getProperty("dbURL");
-        String log = dbConfig.getProperty("log");
-        String additionalConfig = dbConfig.getProperty("config");
+
         try {
             connection = DriverManager.getConnection(dbUrl + log + additionalConfig);
         } catch (SQLException e) {
@@ -36,4 +46,5 @@ public class Database {
     public Connection getConnection(){
         return connection;
     }
+
 }
