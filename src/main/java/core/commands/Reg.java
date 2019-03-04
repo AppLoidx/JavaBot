9ae06f9015dd7b5112bd0478c4fd209aee41fb2f
Arthur Desk <apploidyakutsk@gmail.com>
2019-03-04 22:59:17 +0300

    changes

--- src/main/java/core/commands/Reg.java
+++ src/main/java/core/commands/Reg.java
@@ -1,8 +1,14 @@
 package core.commands;
 
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ClientException;
+import com.vk.api.sdk.objects.messages.Message;
+import com.vk.api.sdk.objects.users.UserXtrCounters;
+import core.commands.VKCommands.VKCommand;
 import core.common.KeysReader;
 import core.common.UserInfoReader;
 import core.modules.UsersDB;
+import vk.MessageSender;
 
 import java.sql.SQLException;
 import java.util.Map;
@@ -11,7 +17,7 @@ import java.util.Map;
  * @version 1.0
  * @author Arthur Kupriyanov
  */
-public class Reg extends Command {
+public class Reg extends Command implements VKCommand {
     public static void main(String[] args) {
         try {
             UsersDB usersDB = new UsersDB();
@@ -63,5 +69,53 @@ public class Reg extends Command {
     @Override
     protected void setConfig() {
         this.commandName = "reg";
+    }
+
+    @Override
+    public String exec(Message message) {
+        MessageSender ms = new MessageSender();
+        UserXtrCounters info = ms.getUserInfo(message.getUserId());
+
+        try {
+            UsersDB usersDB = new UsersDB();
+            int vkid = info.getId();
+            String lastname = info.getLastName();
+            String name = info.getFirstName();
+            String group = null;
+
+            Map<String, String> keyMap = KeysReader.readKeys(message.getBody().split(" "));
+
+
+            if (!usersDB.checkUserExsist(vkid)) {
+                if (keyMap.containsKey("-g")){
+                    group = keyMap.get("-g");
+                } else {
+                    ms.sendMessage("Введите номер вашей группы через ключ -g.\n" +
+                            "Например, reg -g P3112", message.getUserId());
+                    return "";
+                }
+                usersDB.addUser(name, lastname, vkid, group);
+
+                try {
+                    ms.getSendQuery()
+                            .message("Вы успешно зарегистрированы!")
+                            .peerId(message.getUserId())
+                            .attachment("photo-172998024_456239022")
+                            .execute();
+                } catch (ApiException | ClientException e) {
+                    e.printStackTrace();
+                }
+
+                usersDB.closeConnection();
+            } else {
+                return "Вы уже зарегестрированы";
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return "Ошибка при работе с базой данных: " + e.toString();
+        }
+
+
+        return "";
     }
 }
