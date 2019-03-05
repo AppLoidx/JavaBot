d91a2864913a72b9d8ddafd67986ec2035132cda
Arthur Desk <apploidyakutsk@gmail.com>
2019-03-06 02:25:31 +0300

    added javadoc

--- src/main/java/core/Commander.java
+++ src/main/java/core/Commander.java
@@ -4,10 +4,21 @@ import com.vk.api.sdk.objects.messages.Message;
 
 public class Commander {
 
+    /**
+     * Обработка простых текстовых запросов.
+     * @param userInput строковый запрос (может быть с метаданными)
+     * @return обработанный ответ
+     */
     public static String getResponse(String userInput){
         return CommandDeterminant.getCommand(CommandManager.getCommands(), userInput).init(userInput);
     }
 
+    /**
+     * Обработка сообщений, получаемых через сервис Вконтакте. Имеет ряд дополнительной информации,
+     * но не используется для обработки запросов с программ.
+     * @param message сообщение (запрос) пользователя
+     * @return обработанный ответ, "" - не требует отправки, null - не смог обработать
+     */
     public static String getResponse(Message message){
         return CommandDeterminant.getVKCommand(CommandManager.getCommands(), message).exec(message);
     }
