114577df948235c7b842778361b502480bfece68
Arthur Desk <apploidyakutsk@gmail.com>
2019-03-15 23:32:24 +0300

    name field access changed

--- src/main/java/core/commands/Command.java
+++ src/main/java/core/commands/Command.java
@@ -11,7 +11,7 @@ import core.commands.exceptions.CommandHasNoNameException;
  */
 public abstract class Command {
 
-    final String name;
+    private final String name;
     public String commandName;
     {
         setConfig();
