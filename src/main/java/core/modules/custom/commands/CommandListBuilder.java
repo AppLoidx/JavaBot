5af1911b884a08de6180d68a728877e10bdb242b
Arthur Desk <apploidyakutsk@gmail.com>
2019-03-16 13:37:35 +0300

    new pre-release 0.3.0 fixed bug with gson usage

--- src/main/java/core/modules/custom/commands/CommandListBuilder.java
+++ src/main/java/core/modules/custom/commands/CommandListBuilder.java
@@ -1,12 +1,13 @@
 package core.modules.custom.commands;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 /**
  * @author Arthur Kupriyanov
  */
 public class CommandListBuilder {
-    private static final Gson gson = new Gson().newBuilder().create();
+    private static final Gson gson = new GsonBuilder().create();
     public static CommandList build(String json){
         return gson.fromJson(json, CommandList.class);
     }
