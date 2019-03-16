5b5bb7bd6c74a25be83143d190c2aeddaafd8067
Arthur Desk <apploidyakutsk@gmail.com>
2019-03-16 13:27:06 +0300

    added manual

--- src/main/java/core/commands/Version.java
+++ src/main/java/core/commands/Version.java
@@ -1,9 +1,15 @@
 package core.commands;
 
+import core.common.KeysReader;
+import core.modules.Date;
+
+import java.util.Map;
+
 /**
  * @author Arthur Kupriyanov
  */
-public class Version extends Command{
+public class Version extends Command implements Helpable{
+    private static String date = Date.getDate();
     @Override
     protected void setConfig() {
         commandName = "version";
@@ -11,6 +17,27 @@ public class Version extends Command{
 
     @Override
     public String init(String... args) {
-        return "23:40 15.03.2019";
+        Map<String, String> keyMap = KeysReader.readKeys(args);
+
+        if (keyMap.containsKey("-i")){
+            String ver = "";
+            ver += "Добавлена команда timed-command\n";
+            ver += "Добавлена команда news\n";
+            ver += "Добавлены документации к некоторым командам";
+
+            return ver;
+        }
+        return date;
+    }
+
+    @Override
+    public String getManual() {
+        return "По умолчанию возвращает дату деплоя\n" +
+                "С ключом -i возвращает краткое описание изменений";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Информация о версии";
     }
 }
