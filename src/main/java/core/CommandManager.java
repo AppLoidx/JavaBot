bb42c323370ff1cc289e8911e55546bb1dea4044
Arthur Desk <apploidyakutsk@gmail.com>
2019-03-19 15:12:19 +0300

    new Tess command

--- src/main/java/core/CommandManager.java
+++ src/main/java/core/CommandManager.java
@@ -33,6 +33,7 @@ public class CommandManager {
         commands.add(new TimedCommand());
         commands.add(new News());
         commands.add(new Session());
+        commands.add(new Tess());
 
         SessionManager.addMode(new Tracer());
 
