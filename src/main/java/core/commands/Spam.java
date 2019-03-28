c32cac7f11cfb0ae89687bca88a46938b76bb64f
Arthur Desk <apploidyakutsk@gmail.com>
2019-03-28 18:44:16 +0300

    added photo support

--- src/main/java/core/commands/Spam.java
+++ src/main/java/core/commands/Spam.java
@@ -1,13 +1,19 @@
 package core.commands;
 
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.objects.messages.Message;
+import com.vk.api.sdk.objects.messages.MessageAttachment;
+import com.vk.api.sdk.objects.messages.MessageAttachmentType;
 import com.vk.api.sdk.objects.users.UserXtrCounters;
+import com.vk.api.sdk.queries.messages.MessagesSendQuery;
 import core.commands.VKCommands.VKCommand;
 import core.common.KeysReader;
 import core.modules.UsersDB;
 import vk.VKManager;
 
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -40,6 +46,7 @@ public class Spam extends Command implements VKCommand {
 
         UserXtrCounters user = VKManager.getUserInfo(message.getUserId());
         if (user != null) {
+            if (!keyMap.containsKey("-a") || message.getUserId()!=255396611)
             msg += "\nСообщение от: " + user.getFirstName() + " " + user.getLastName();
         }
 
@@ -47,8 +54,9 @@ public class Spam extends Command implements VKCommand {
         try {
             Map<Integer, String> users = usersDB.getVKIDListWithGroup();
             for (int vkid : users.keySet()){
+                if (group.equals("*")) group =".*";
                 if (users.get(vkid).equals(group) || users.get(vkid).matches(group)){
-                    sendMessage(vkid, msg);
+                    sendMessage(vkid, msg, message);
                 }
             }
             return "Хорошо!";
@@ -59,7 +67,26 @@ public class Spam extends Command implements VKCommand {
 
     }
 
-    private void sendMessage(int vkid, String msg){
-        new VKManager().sendMessage(msg, vkid);
+    private void sendMessage(int vkid, String msg, Message vkMessage){
+        MessagesSendQuery query = new VKManager().getSendQuery().peerId(vkid).message(msg);
+
+        List<MessageAttachment> attachments = vkMessage.getAttachments();
+        if (attachments != null) {
+            for (MessageAttachment a : attachments) {
+                MessageAttachmentType attach = a.getType();
+                if (attach == MessageAttachmentType.PHOTO) {
+                    query.attachment(attach.getValue() + a.getPhoto().getOwnerId() + "_" + a.getPhoto().getId() + "_" + a.getPhoto().getAccessKey());
+                }
+                if (attach == MessageAttachmentType.DOC) {
+                    query.attachment(a.getDoc().getUrl());
+                }
+            }
+        }
+
+        try {
+            query.execute();
+        } catch (ApiException | ClientException e) {
+            e.printStackTrace();
+        }
     }
 }
